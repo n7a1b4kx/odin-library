02ef2a8441e08e39258c3dcea9a9f50c6bcd6db3
n7a1b4kx <169384643+n7a1b4kx@users.noreply.github.com>
2025-03-11 11:27:21 +0500

    add: book object constructor

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const myLib = [];
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}.`;
+    };
+}
+
+function addBookToLib() {
+
+}
